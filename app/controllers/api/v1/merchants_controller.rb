d0f24e2654e0f34d43208a820a556bd3168f7492
Joop Stark <stark.joop@gmail.com>
2023-10-16 16:01:36 -0600

    add: controllers

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::MerchantsController < Api::V1::ApplicationController
+  def index
+  end
+  
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: :index
+      end
+      resources :items do
+        resources :merchants, only: :index
+      end
+      get '/merchants/single_search', to: 'merchants#single_search'
+      get '/items/search', to: 'items#search'
+    end
+  end
 end
