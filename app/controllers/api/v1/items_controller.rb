6885d5303911e189d66311033d69d23ed1c1af14
Joop Stark <stark.joop@gmail.com>
2023-10-16 23:15:43 -0600

    setup: serializers: items and customers

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::ItemsController < Api::V1::ApplicationController
   def index
+    if params[:merchant_id]
+      render json: ItemSerializer.new(Merchant.find_by_id(params[:merchant_id]).items)
+    else
+      render json: ItemSerializer.new(Item.all)
+    end
   end
 
   def create
   end
 
   def show
+    render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
   def update
--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::MerchantsController < Api::V1::ApplicationController
   def index
+    render json: MerchantSerializer.new(Merchant.all)
   end
   
   def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
   end
 end
